e1f5f3029607ab7d1c1d031f252885e671922b3c
vince Wen <15007193272@163.com>
2018-03-18 21:15:18 +0800

    code modified,-e finished

--- src/java86/Javatest.java
+++ src/java86/Javatest.java
@@ -1,57 +1,151 @@
 package java86;
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Javatest {
 
 
-    static int num_sum = 0;
-    static  int word_sum = 0;
-    static int sym_sum = 0;
-    static int sum = 0;
 
-    static int count = 0;
 
-    public static long xermote(String fileName) throws IOException {
+
+
+    public static int countC(String fn){
         BufferedReader reader = null;
+        int res=0;
+        try {
+            reader = new BufferedReader(new FileReader(fn));
+            String str = null;
+            while ((str = reader.readLine()) != null) {
 
-        reader = new BufferedReader(new FileReader(fileName));
-        String str = null;
-
-        PrintStream ps = new PrintStream("F:/Users/MSI/log.txt");
-
-        while ((str = reader.readLine()) != null) {
-            count++;
-            word_sum += str.split(" |,").length;
-            num_sum += str.length();
+                res += str.length();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        return res;
 
+    }
 
+    public static int countW(String fn,List<String> list){
+        BufferedReader reader = null;
+        int res=0;
+        try {
+            reader = new BufferedReader(new FileReader(fn));
+            String str = null;
+            while ((str = reader.readLine()) != null) {
+                String[]temp=str.split(" |,");
+                for(String s:temp){
+                    if(s!=null&&!list.contains(s)){
+                        ++res;
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return res;
+    }
 
-        System.setOut(ps);
-
-        System.out.println("字符数量" + num_sum);
-        System.out.println("单次数量" + word_sum);
-        System.out.println("行数" + count);
-        sum = sym_sum + num_sum + word_sum;
-        System.out.println("总数" + sum);
-
-        System.out.println("这行语句将会被写到log.txt文件中");
-        return sum;
+    public static int countL(String fn){
+        BufferedReader reader = null;
+        int res=0;
+        try {
+            reader = new BufferedReader(new FileReader(fn));
+            String str = null;
+            while ((str = reader.readLine()) != null) {
+                ++res;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return res;
     }
 
 
+    public static String buildRes(String fn,char c,int res){
+        StringBuilder sb=new StringBuilder();
+        switch (c) {
+            case 'c':
+                sb.append(fn+",字符数"+res+"\n");
+                break;
+            case 'w':
+                sb.append(fn+",单词数"+res+"\n");
+                break;
+            case 'l':
+                sb.append(fn+",行数"+res+"\n");
+                break;
+        }
+        return sb.toString();
+    }
+
+    public static List<String> read(String fn){
+        List<String> list=new ArrayList<String>();
+
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(fn));
+            StringBuilder sb=new StringBuilder();
+            String s=null;
+            while((s=reader.readLine())!=null){
+                sb.append(s);
+                sb.append(" ");
+            }
+            String[] arr=sb.toString().split(" ");
+            Arrays.stream(arr).forEach(x->{
+                list.add(x);
+            });
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
+    }
+
         public static void main(String[] args) {
 
-            final String[] p = {"-c", "-a", "-l", "-w", "-o"};
+            int cNum=0,lNum=0,wNum=0;
+            List<String>list=new LinkedList<String>();
+            final String[] p = {"-c", "-a", "-l", "-w", "-o","-s","-e"};
             Scanner sc = new Scanner(System.in);
-            System.out.println("ScannerTest, Please Enter Name:");
+            System.out.println("Please Enter Your Command:");
             String cmd = sc.nextLine();
             sc.close();
             String[] parame = cmd.split(" ");
             int len = parame.length;
-
             String inName = null;
 
             for (int i = 1; i < len; ++i) {
@@ -67,28 +161,44 @@ public class Javatest {
                     break;
                 }
             }
-
             String outName = null;
+            StringBuilder res=new StringBuilder();
+
             try {
-                xermote(inName);
-                String result = "";
                 for (int i = 1; i < len; ++i) {
-
                     if (parame[i].equals("-c")) {
-                        result += "字符数量："+Javatest.num_sum+"/n";
+                       cNum=countC(inName);
+                       res.append(buildRes(inName,'c',cNum));
                     } else if (parame[i].equals("-l")) {
-
+                        lNum=countL(inName);
+                        res.append(buildRes(inName,'l',lNum));
                     } else if (parame[i].equals("-w")) {
-
+                        if(list.size()==0){
+                            for(int j=i+1;j<len;++j){
+                                if(parame[j].equals("-e")){
+                                    list=read(parame[j+1]);
+                                    break;
+                                }
+                            }
+                        }
+                        wNum=countW(inName,list);
+                        res.append(buildRes(inName,'w',wNum));
                     } else if (parame[i].equals("-o")) {
                         outName = parame[i + 1];
                     } else if (parame[i].equals("-e")) {
                         String stopFile = parame[i + 1];
+                        list=read(stopFile);
+                    } else if (parame[i].equals("-s")) {
+
+                    } else if (parame[i].equals("-a")) {
+
                     }
                 }
+
                 outName = outName == null ? "result.txt" : outName;
+
                 FileWriter writer = new FileWriter(outName);
-                writer.write(result);
+                writer.write(res.toString());
                 writer.close();
 
             } catch (IOException e) {
